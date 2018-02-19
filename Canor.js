46aa84e0d57284e4c0e2ed4a82e1c3f367f6d371
Joseph Schefer <joseph.schefer@gmail.com>
2018-02-19 17:06:47 -0500

    EH?

--- Canor.js
+++ Canor.js
@@ -1,2 +1,12 @@
 // all code written in ES6
 // although it uses jQuery syntax, this library will not use jQuery
+
+class SoundObject {
+  constructor() {
+
+  }
+
+  set source(src) {
+    return this.src = src;
+  }
+}
